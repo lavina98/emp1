3ae0ae6b6054a5f4763f01424134df541682ddf2
Rohit <sawardekar.rohit078@gmail.com>
2018-06-09 14:35:29 +0530

    final

--- src/pages/mymanager/mymanager.ts
+++ src/pages/mymanager/mymanager.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PaymentOptionsPage } from '../payment-options/payment-options';
-
+import { Http,RequestOptions,Headers} from "@angular/http";
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the MymanagerPage page.
  *
@@ -16,15 +17,49 @@ import { PaymentOptionsPage } from '../payment-options/payment-options';
 })
 export class MymanagerPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  http:any;
+  hash:any;
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams
+              ,http:Http,
+              private storage:Storage) {
+    this.http=http;
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MymanagerPage');
+    this.storage.get('user').then((user)=>{
+      console.log(user);}
+    );
   }
 
   apply()
   {
-    this.navCtrl.push(PaymentOptionsPage,{val:1000})
+    this.storage.get('Hash').then((hash) => {
+      this.storage.get('user').then((user)=>{
+        console.log(user);
+      
+      this.hash = hash;
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': hash
+    });
+    let body=JSON.stringify({
+        emp_id:user.id
+    });
+    let options = new RequestOptions({ headers: headers });
+    this.http.post('http://www.forehotels.com:3000/api/mymanager',body,options).subscribe(
+      (data)=>{
+        console.log(data.json());
+      },
+      (err)=>{
+        console.log(err);
+      }
+    );
+
+     this.navCtrl.push(PaymentOptionsPage,{val:1000})
+      });
+  });
   }
 }
